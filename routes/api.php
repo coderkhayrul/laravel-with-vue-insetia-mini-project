ae67ee0d0844f786a25c2fcc4197ffc9e634e4c3
coderkhayrul <coderkhayrul@gmail.com>
2025-03-07 16:28:39 +0600

    User Santum for API Token Authentication

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TaskController;
 use App\Http\Controllers\Api\V1\CompleteTaskController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 
 
 
@@ -12,6 +15,16 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
 
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+    Route::post('/register', RegisterController::class);
+});
+
+
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
