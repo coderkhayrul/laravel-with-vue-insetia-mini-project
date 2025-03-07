d273dde2f6b56acea7371492725724ca792f4e03
coderkhayrul <coderkhayrul@gmail.com>
2025-03-07 16:06:07 +0600

    Laravel With Vue Inertia SPA Application Test

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\V1\CompleteTaskController;
+
+
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+    Route::apiResource('/tasks', TaskController::class);
+    Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
